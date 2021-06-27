dfd1a029fc2ebe6452759002e5b8f49d972ed131
Deepak Surya <ideepaksuryas@gmail.com>
2021-06-27 19:30:08 +0530

    refactor to separate func, clear input on submit

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const myLeads = [1, 2, 3];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
+const myLeads = [];
+const inputEl = document.getElementById('input-el');
+const inputBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el');
 
-inputBtn.addEventListener("click", handleSave);
+inputBtn.addEventListener('click', handleSave);
 
 function handleSave() {
-  console.log("Button clicked!");
+  myLeads.push(inputEl.value);
+  inputEl.value = '';
+  renderLeads();
 }
 
-let listItems = '';
-for(let i = 0; i < myLeads.length; i += 1) {
-  listItems += "<li>" + myLeads[i] + "</li>"
-}
+function renderLeads() {
+  let listItems = '';
+  for(let i = 0; i < myLeads.length; i += 1) {
+    listItems += '<li>' + myLeads[i] + '</li>';
+  }
 
-ulEl.innerHTML = listItems;
+  ulEl.innerHTML = listItems;
+}
