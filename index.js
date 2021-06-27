bcfe377b1c909036f54a2ebabef778850246fa8f
Deepak Surya <ideepaksuryas@gmail.com>
2021-06-27 21:57:05 +0530

    add delete to clear local store

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
+const deleteBtn = document.getElementById('delete-btn');
 
 inputBtn.addEventListener('click', handleSave);
+deleteBtn.addEventListener('click', clearLocalStorage);
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
 if (leadsFromLocalStorage) {
@@ -15,6 +17,12 @@ function handleSave() {
   myLeads.push(inputEl.value);
   inputEl.value = '';
   localStorage.setItem('myLeads', JSON.stringify(myLeads));
+  renderLeads();
+}
+
+function clearLocalStorage() {
+  localStorage.clear();
+  myLeads = [];
   renderLeads();
 }
 
